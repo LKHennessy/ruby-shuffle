37194bc52ea315b2599e3e70836d88a3780297da
Lauren Hennessy <lauren.hennessy.work@gmail.com>
2019-05-11 19:20:36 +0800

    class and better naming

--- shuffle.rb
+++ shuffle.rb
@@ -1,35 +1,37 @@
 require 'rainbow'
 require 'chroma'
 
-SEED = Chroma.paint 'hsl(0, 100%, 50%)'
-CARD = '[]'
+class Shuffle
+  SEED = Chroma.paint 'hsl(0, 100%, 50%)'
+  CARD = '[]'
 
-Chroma.define_palette :rainbow_52_palette do
-  51.times do |n|
-    spin((n + 1) * 6)
+  Chroma.define_palette :rainbow_52_palette do
+    51.times do |n|
+      spin((n + 1) * 6)
+    end
   end
-end
 
-rainbow = SEED.palette.rainbow_52_palette.map(&:to_hex)
+  deck = SEED.palette.rainbow_52_palette.map(&:to_hex)
 
-rainbow.each do |colour|
-  puts Rainbow(CARD).color(colour)
-end
+  deck.each do |colour|
+    puts Rainbow(CARD).color(colour)
+  end
 
-puts '-----------------RIFFLE SHUFFLE-----------------'
+  puts '-----------------RIFFLE SHUFFLE-----------------'
 
-puts '-----------------Split the deck-----------------'
+  puts '-----------------Split the deck-----------------'
 
-left_half, right_half = rainbow.each_slice(26).to_a
+  left_half, right_half = deck.each_slice(26).to_a
 
-left_half.each_with_index do |colour, i|
-  puts "#{Rainbow(CARD).color(colour)}  #{Rainbow(CARD).color(right_half[i])}"
-end
+  left_half.each_with_index do |colour, i|
+    puts "#{Rainbow(CARD).color(colour)}  #{Rainbow(CARD).color(right_half[i])}"
+  end
 
-puts '-----------------Intersperse-----------------'
+  puts '-----------------Intersperse-----------------'
 
-shuffled_deck = [left_half, right_half].transpose.flatten
+  shuffled_deck = [left_half, right_half].transpose.flatten
 
-shuffled_deck.each do |colour|
-  puts Rainbow(CARD).color(colour)
+  shuffled_deck.each do |colour|
+    puts Rainbow(CARD).color(colour)
+  end
 end
