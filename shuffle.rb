2bf80cbfc2a352ce28f22ba4ee52dd96994599a4
Lauren Hennessy <lauren.hennessy.work@gmail.com>
2019-05-11 20:04:11 +0800

    print horizontally

--- shuffle.rb
+++ shuffle.rb
@@ -3,35 +3,42 @@ require 'chroma'
 
 class Shuffle
   SEED = Chroma.paint 'hsl(0, 100%, 50%)'
-  CARD = '[]'
+  CARD = ' '
 
   Chroma.define_palette :rainbow_52_palette do
     51.times do |n|
-      spin((n + 1) * 6)
+      spin((n + 1) * 6.7)
     end
   end
 
-  deck = SEED.palette.rainbow_52_palette.map(&:to_hex)
-
-  deck.each do |colour|
-    puts Rainbow(CARD).color(colour)
+  def self.print_horizontal(cards)
+    string = ''
+    cards.each do |card|
+      string = string + card
+    end
+    puts string
   end
 
+  rainbow = SEED.palette.rainbow_52_palette.map(&:to_hex)
+
   puts '-----------------RIFFLE SHUFFLE-----------------'
+
+  deck = rainbow.map do |colour|
+    Rainbow(CARD).background(colour)
+  end
+
+  self.print_horizontal(deck)
 
   puts '-----------------Split the deck-----------------'
 
   left_half, right_half = deck.each_slice(26).to_a
 
-  left_half.each_with_index do |colour, i|
-    puts "#{Rainbow(CARD).color(colour)}  #{Rainbow(CARD).color(right_half[i])}"
-  end
+  self.print_horizontal(left_half)
+  self.print_horizontal(right_half)
 
   puts '-----------------Intersperse-----------------'
 
   shuffled_deck = [left_half, right_half].transpose.flatten
 
-  shuffled_deck.each do |colour|
-    puts Rainbow(CARD).color(colour)
-  end
+  self.print_horizontal(shuffled_deck)
 end
